1ce06e2f09e8999008d56acb5c38cb32c4d10736
amilamanoj <amilamanoj@022b4951-5e73-82fb-9d0e-3a27e9f3033c>
2012-01-30 11:41:44 +0000

    adding .thrift file for siyapath with basic service definition for gossip based communication

---
+++ modules/core/src/main/thrift/siyapath.thrift
@@ -0,0 +1,26 @@
+
+namespace java siyapath
+
+/**
+ * Contains information about a particular node
+ */
+struct NodeData {
+  1: string timestamp,
+  2: map<string,string> nodeData
+}
+
+/**
+ * Data exchanged during gossip communication between two nodes
+ */
+struct GossipData {
+  1: map<string, NodeData> gossipData
+}
+
+/**
+ * Service for communication based on gossip protocols
+ */
+service GossipService {
+
+  GossipData Gossip(1:GossipData gData)
+
+}
