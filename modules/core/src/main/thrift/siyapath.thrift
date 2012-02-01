59dc20271c146daa9239e902ed3309b0a02a9969
nadeeshani.hewage88@gmail.com <nadeeshani.hewage88@gmail.com@022b4951-5e73-82fb-9d0e-3a27e9f3033c>
2012-02-01 14:19:47 +0000

    Adding MemberDiscoveryService

--- modules/core/src/main/thrift/siyapath.thrift
+++ modules/core/src/main/thrift/siyapath.thrift
@@ -5,29 +5,34 @@ namespace java siyapath
  * Contains information about a particular node
  */
 struct NodeData {
-  1: string timestamp,
-  2: map<string,string> nodeData
+    1: string timestamp,
+    2: map<string,string> nodeData
 }
 
 /**
  * Data exchanged during gossip communication between two nodes
  */
 struct GossipData {
-  1: map<string, NodeData> gossipData
+    1: map<string, NodeData> gossipData
 }
 
 /**
  * Service for communication based on gossip protocols
  */
 service GossipService {
+    GossipData Gossip(1:GossipData gData)
+}
 
-  GossipData Gossip(1:GossipData gData)
-
+/**
+ * Service to get members of the node
+ */
+service MemberDiscoveryService {
+    list<string> members(1:String nodeID)
 }
 
 /**
  * Service for getting current set of known members
  */
 service MemberService{
-  set<string> getMembers()
+    set<string> getMembers()
 }
