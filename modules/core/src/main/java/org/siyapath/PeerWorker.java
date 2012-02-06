4c480257284bb8ee0a9332553957cf67e0b9ac82
amilamanoj <amilamanoj@022b4951-5e73-82fb-9d0e-3a27e9f3033c>
2012-02-06 20:00:06 +0000

    implementing getMembers() operation. updating node context to hold bootstrap info. new IDL operation notifyPresense() and its implementation. so the bootstrapper node get notified when a new node joins the network. newly joined node can get a list of members from the bootstrapper node

--- modules/core/src/main/java/org/siyapath/PeerWorker.java
+++ modules/core/src/main/java/org/siyapath/PeerWorker.java
@@ -1,6 +1,16 @@
 package org.siyapath;
 
 
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+import java.net.ConnectException;
+import java.util.Set;
+
 /**
  * Created by IntelliJ IDEA.
  * User: amila
@@ -16,15 +26,33 @@ public class PeerWorker {
         this.nodeContext = nodeContext;
     }
 
-    public void work() {
-        if (!nodeContext.membersExist()) {
-            initiateMembers();
-        }
+    public void start() {
         new WorkerThread().start();
     }
 
     private void initiateMembers() {
+        TTransport transport = new TSocket("localhost", FrameworkInformation.BOOTSTRAP_PORT);
+        try {
+            transport.open();
+            TProtocol protocol = new TBinaryProtocol(transport);
+            GossipService.Client client = new GossipService.Client(protocol);
+            Set newNodes = client.getMembers();
+            for (Object n: newNodes){
+                String node = (String) n;
+                nodeContext.addMember(Integer.parseInt(node));
+            }
 
+        } catch (TTransportException e) {
+            if (e.getCause() instanceof ConnectException) {
+                System.out.println("Could not connect to the bootstrapper :(");
+            }
+
+        } catch (TException e) {
+            e.printStackTrace();
+
+        } finally {
+            transport.close();
+        }
     }
 
     private class WorkerThread extends Thread {
@@ -35,9 +63,19 @@ public class PeerWorker {
         public void run() {
 
             isRunning = true;
-
             while (isRunning) {
-
+                if (!nodeContext.isBootstrapper()){
+                if (!nodeContext.membersExist()) {
+                    System.out.println("No known members. Contacting the bootstrapper to get some nodes");
+                    initiateMembers();
+                }
+                }
+                System.out.println("Number of members: " + nodeContext.getMemeberCount());
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
--- modules/core/src/main/java/org/siyapath/SiyapathNode.java
+++ modules/core/src/main/java/org/siyapath/SiyapathNode.java
@@ -5,6 +5,9 @@ package org.siyapath;
  * Time: 4:44 PM
  */
 
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
@@ -17,15 +20,16 @@ public class SiyapathNode {
     private GossipServiceHandler handler;
     private GossipService.Processor processor;
     private PeerListener peerListener;
+    private PeerWorker peerWorker;
     private NodeContext nodeContext;
     private int nodePort;
 
 
     public SiyapathNode() {
         nodeContext = new NodeContext();
-        handler = new GossipServiceHandler();
+        handler = new GossipServiceHandler(nodeContext);
         processor = new GossipService.Processor(handler);
-        nodePort = -1;
+        nodePort = new Random().nextInt(1000) + 9021;
 
     }
 
@@ -41,16 +45,17 @@ public class SiyapathNode {
 
             if (!connectToBootStrapper()) {
                 System.out.println("OK, I'm gonna be the bootstrapper");
+                nodeContext.setBootstrapper(true);
                 this.nodePort = FrameworkInformation.BOOTSTRAP_PORT;
             } else {
                 System.out.println("Bootstrapper is up and running");
-
-                this.nodePort = new Random().nextInt(1000) + 9021;
             }
 
             peerListener = new PeerListener(processor, this.nodePort);
-
             peerListener.start();
+            
+            peerWorker = new PeerWorker(nodeContext);
+            peerWorker.start();
 
         } catch (Exception x) {
             x.printStackTrace();
@@ -59,16 +64,21 @@ public class SiyapathNode {
 
     private boolean connectToBootStrapper() {
         boolean isBootStrapperAlive = false;
-        System.out.println("Tring to connect to a bootstrapper node");
+        System.out.println("Trying to connect to a bootstrapper node");
         TTransport transport = new TSocket("localhost", FrameworkInformation.BOOTSTRAP_PORT);
         try {
             transport.open();
+            TProtocol protocol = new TBinaryProtocol(transport);
+            GossipService.Client client = new GossipService.Client(protocol);
+            client.notifyPresence(nodePort);
             isBootStrapperAlive = true;
         } catch (TTransportException e) {
 //            e.printStackTrace();
             if (e.getCause() instanceof ConnectException) {
                 System.out.println("Could not connect to the bootstrapper :(");
             }
+        } catch (TException e) {
+            e.printStackTrace();
         } finally {
             transport.close();
         }
