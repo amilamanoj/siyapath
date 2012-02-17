948e41618634408a1625c148786d32205a49faaa
amilamanoj <amilamanoj@022b4951-5e73-82fb-9d0e-3a27e9f3033c>
2012-02-17 09:08:48 +0000

    NodeContext javadoc

--- trunk/modules/core/src/main/java/org/siyapath/NodeContext.java
+++ trunk/modules/core/src/main/java/org/siyapath/NodeContext.java
@@ -5,9 +5,15 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-
+/**
+ * The NodeContext holds runtime information
+ * for a Siyapath node instance. This is a singleton
+ */
 public class NodeContext {
 
+    /**
+     * List of known member nodes
+     */
     private ArrayList<Integer> members;
     private boolean isBootstrapper;
     private boolean isBackup;
@@ -18,6 +24,10 @@ public class NodeContext {
         this.isBootstrapper = false;
     }
 
+    /**
+     * Returns the instance of this class
+     * @return NodeContext instance
+     */
     public static NodeContext getInstance() {
         if (instance == null) {
             instance = new NodeContext();
@@ -25,10 +35,19 @@ public class NodeContext {
         return instance;
     }
 
-    public Integer getMember(int index) {
-        return members.get(index);
+    /**
+     * Returns a particular member from the member list
+     * @param id member id
+     * @return a member node
+     */
+    public Integer getMember(int id) {
+        return members.get(id);
     }
 
+    /**
+     * Returns a random member from list of know members
+     * @return a random member node
+     */
     public Integer getRandomMember() {
         if (members.isEmpty()) {
             return null;
@@ -36,18 +55,34 @@ public class NodeContext {
         return members.get(new Random().nextInt(members.size()));
     }
 
-    public void addMember(int port) {
-        members.add(new Integer(port));
+    /**
+     * Adds a new member
+     * @param id member id
+     */
+    public void addMember(int id) {
+        members.add(new Integer(id));
     }
 
+    /**
+     * Returns the size of member list
+     * @return number of members
+     */
     public int getMemeberCount() {
         return members.size();
     }
 
+    /**
+     * Returns whether members exist or not
+     * @return true or false
+     */
     public boolean membersExist() {
         return (members.size() > 0) ? true : false;
     }
 
+    /**
+     * Returns the the set of member
+     * @return member set
+     */
     public Set<Integer> getMemberSet() {
         Set<Integer> nodeSet = new HashSet<Integer>();
         for (Integer nodeID : members) {
@@ -56,10 +91,18 @@ public class NodeContext {
         return nodeSet;
     }
 
+    /**
+     * checks whether this node is the bootstrapper
+     * @return true or false
+     */
     public boolean isBootstrapper() {
         return isBootstrapper;
     }
 
+    /**
+     * Declare this node as the bootstrapper
+     * @param bootstrapper true or false
+     */
     public void setBootstrapper(boolean bootstrapper) {
         isBootstrapper = bootstrapper;
     }
