d0038fdd79995f229af7d6c46c2b3d66f962a0ee
nadeeshani.hewage88@gmail.com <nadeeshani.hewage88@gmail.com@022b4951-5e73-82fb-9d0e-3a27e9f3033c>
2012-03-14 08:58:10 +0000

    Adding means to distribute a task to a random node.

--- trunk/modules/core/src/main/java/org/siyapath/job/TaskDistributor.java
+++ trunk/modules/core/src/main/java/org/siyapath/job/TaskDistributor.java
@@ -8,12 +8,16 @@ import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
+import org.siyapath.FrameworkInformation;
 import org.siyapath.Siyapath;
+import org.siyapath.SiyapathNode;
 import org.siyapath.Task;
 
 import java.io.*;
 import java.net.ConnectException;
 import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.Random;
 
 
 /*
@@ -24,26 +28,65 @@ public class TaskDistributor {
     
     private static final Log log = LogFactory.getLog(TaskDistributor.class);
     Task task = null;
+//    private Map<Integer,Task> taskProcessingNodes = null;   //nodeID of processing node and task
+    private Map<SiyapathNode, Task> taskProcessingNodes = null;
+    private Map<Integer,Task> tasks = null;   //taskID and task
 
+    TaskDistributor(Task task){
+        tasks.put(task.taskID,task);
+    }
     /*
     * main method is for demo purpose only, until user job submission is implemented.
     * */
     public static void main(String[] args) {
-        TaskDistributor taskDistributor = new TaskDistributor();
-        taskDistributor.send();
+//        TaskDistributor taskDistributor = new TaskDistributor();
+//        taskDistributor.send();
     }
 
     public Task createTask(){
 
         //temporary values have been set.
         try {
-            task = new Task("123","234",convertFileToByteBuffer(),"Sending a Temp task data in a String.","org.test.siyapath.CalcDemo");
+            task = new Task(123, 234, convertFileToByteBuffer(),"Sending a Temp task data in a String.","org.test.siyapath.CalcDemo");
             log.info("Created a task out of parameters submitted by user.");
         } catch (IOException e) {
             log.warn("Could not create a task out of given parameters.");
             e.printStackTrace();
         }
     return task;
+    }
+    
+    public int pickOneProcessingNode(){
+
+        TTransport transport = new TSocket("localhost", FrameworkInformation.BOOTSTRAP_PORT);
+        int pickedProcessingNode = 0;
+        Object[]  memberArray = null;
+        try{
+            transport.open();
+            TProtocol protocol = new TBinaryProtocol(transport);
+            Siyapath.Client client = new Siyapath.Client(protocol);
+            /*
+            * This is for the moment only, actually the task distributing node will maintain a set of member nodes as
+            * every node does, and pick one out of them who has matching resources
+            * */
+            memberArray = client.getMembers().toArray();
+            pickedProcessingNode = (Integer)memberArray[new Random().nextInt(memberArray.length)];
+
+            log.info("Targeting processing node with node ID :" + pickedProcessingNode);
+
+//            if(taskProcessingNodes==null){
+//                taskProcessingNodes = new HashMap<SiyapathNode,Task>();
+//            }
+//            taskProcessingNodes.put(new Integer(pickedProcessingNode),createTask());//
+
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        } catch (TException e) {
+            e.printStackTrace();
+        }
+
+    return pickedProcessingNode;
+
     }
 
     public void send(){
--- trunk/modules/user/src/main/java/org/siyapath/client/UserHandler.java
+++ trunk/modules/user/src/main/java/org/siyapath/client/UserHandler.java
@@ -12,14 +12,11 @@ import org.siyapath.FrameworkInformation;
 import org.siyapath.Siyapath;
 import org.siyapath.Task;
 
-import javax.swing.*;
-import java.io.*;
 import java.net.ConnectException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 
 public class UserHandler {
     private static final Log log = LogFactory.getLog(UserHandler.class);
@@ -57,7 +54,7 @@ public class UserHandler {
             Object memberArray[] =client.getMembers().toArray();
             log.info("Number of members: "+memberArray.length);
             Integer selectedMember = (Integer) memberArray[new Random().nextInt(memberArray.length)];
-            Task task = new Task("123","234",convertFileToByteBuffer(fileName),"Sending a Temp task data in a String.","org.test.siyapath.CalcDemo");
+            Task task = new Task(123, 234, ByteBuffer.wrap(new byte[10]),"Sending a Temp task data in a String.","org.test.siyapath.CalcDemo");
             Map job = new HashMap<Integer, Task>();
             job.put(1,task);
             log.info("Sending new job to node: "+selectedMember);
@@ -70,45 +67,10 @@ public class UserHandler {
         } catch (TException e) {
             res = "tEx";
             e.printStackTrace();
-        } catch (IOException e) {
-            log.warn("Could not create a task out of given parameters.");
-            e.printStackTrace();
         } finally {
             transport.close();
         }
     }
 
-    //TODO: this method is replicated. move to common utils
-    public ByteBuffer convertFileToByteBuffer(String fileLocation) throws IOException {
-        File file = new File(fileLocation);
-        InputStream inputStream = null;
-
-        byte[] bytes = new byte[(int)file.length()];
-        if (file.length() > Integer.MAX_VALUE) {
-            log.error("File is too large.");
-        }
-        try{
-            inputStream = new BufferedInputStream(new FileInputStream(file));
-            int offset=0, numRead;
-
-            while (offset < bytes.length
-                    && (numRead=inputStream.read(bytes, offset, bytes.length-offset)) >= 0) {
-                offset += numRead;
-            }
-            // Ensure all the bytes have been read
-            if (offset < bytes.length) {
-                log.warn("Could not completely read file " + file.getName());
-                throw new IOException("Could not completely read file " + file.getName());
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null) {
-                inputStream.close();
-            }
-        }
-        return ByteBuffer.wrap(bytes);
-    }
+    
 }
