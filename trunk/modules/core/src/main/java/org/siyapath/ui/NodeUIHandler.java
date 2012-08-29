b0f2a9b8e1669c7d6ff416fa5c3a1f478fae8f4e
mmalithh <mmalithh@022b4951-5e73-82fb-9d0e-3a27e9f3033c>
2012-08-29 13:45:48 +0000

    Adding Gossip freq & Dispatch freq

--- trunk/modules/core/src/main/java/org/siyapath/ui/NodeUIHandler.java
+++ trunk/modules/core/src/main/java/org/siyapath/ui/NodeUIHandler.java
@@ -76,9 +76,8 @@ public class NodeUIHandler {
                 }
 
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(10);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
             }
 
