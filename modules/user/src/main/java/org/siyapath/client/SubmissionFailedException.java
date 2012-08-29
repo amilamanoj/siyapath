c41a867d6b2cdece4398e5a08e812915a3feb30f
amilamanoj <amilamanoj@022b4951-5e73-82fb-9d0e-3a27e9f3033c>
2012-08-29 18:02:33 +0000

    apidocs for user module. npe fix in sample module

--- modules/user/src/main/java/org/siyapath/client/SubmissionFailedException.java
+++ modules/user/src/main/java/org/siyapath/client/SubmissionFailedException.java
@@ -1,5 +1,8 @@
 package org.siyapath.client;
 
+/**
+ * Thrown by UserHandler when job submission has failed
+ */
 public class SubmissionFailedException extends Exception {
     public SubmissionFailedException(String message, Throwable cause) {
         super(message, cause);
